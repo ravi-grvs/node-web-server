b2ef64b5081606319ee2802c20f6bd1fa9189c02
rk1005 <rkumarkurva@gmail.com>
2017-10-26 22:30:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('Caps_it', (text)=>{
+    return text.toUpperCase()
+});
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} : ${req.path}`;
+    fs.appendFile('server.log',log + '\n', (err)=>{
+        if(err){
+            console.log('unable to log the file');
+        };
+    });
+    console.log(`${now} : ${req.method} : ${req.path}`)
+    next();
+});
+
+// app.use((req, res,next)=>{
+//         res.render('maintenance.hbs');
+//     });
+    
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+    res.render('home.hbs',{
+        pageTitle : 'Home page',
+        welcomeMessage : "welcome to my website"
+        
+    });
+});
+
+app.get('/about', function(req,res){
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+        welcomeMessage : 'This is about page'
+        
+    });
+});
+
+
+app.get('/bad', function(req,res){
+    res.send({
+        errorMessage : "Error page not found"
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log('server is running at port 3000');
+});
